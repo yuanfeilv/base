c17d679e2e7ca4999a90ab45bdd8bc5fe36672df
yuanfei.lv <yuanfei.lv>
2020-10-12 18:07:18 +0800

    初始化

---
+++ zookeeper/src/main/java/com/ambity/zookeeper/Zktest.java
@@ -0,0 +1,10 @@
+package com.ambity.zookeeper;
+
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.server.ZooKeeperServer;
+
+public class Zktest {
+    public static void main(String[] args) {
+        ZooKeeper zooKeeper = new ZooKeeper();
+    }
+}
